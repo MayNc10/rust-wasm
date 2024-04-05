89698821099fadc4f27db8f1aee30aa875e1ea47
May Neelon <mayflowerc10@gmail.com>
2024-04-05 13:33:07 -0400

    First Commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use anyhow::Result;
+use wasmtime::*;
+
+fn main() -> Result<()> {
+    println!("Compiling module...");
+    let engine = Engine::default();
+    let module = Module::from_file(&engine, "src/hello.wat")?; 
+
+    println!("Initializing...");
+    let mut store = Store::new(
+        &engine,
+        ()
+    ); 
+
+    println!("Creating callback...");
+    let hello_func = Func::wrap(&mut store, |_caller: Caller<'_, ()>| {
+        println!("Calling back...");
+    }); 
+
+    println!("Instantiating module...");
+    let imports = [hello_func.into()];
+    let instance = Instance::new(&mut store, &module, &imports)?;
+
+    println!("Extracting export...");
+    let run = instance.get_typed_func::<(), ()>(&mut store, "run")?; 
+
+    println!("Calling export...");
+    run.call(&mut store, ())?; 
+
+    println!("Done.");
+    Ok(())
+}
---
+++ src/wasm_functions.rs
@@ -0,0 +1,10 @@
+use lazy_static::lazy_static;
+use wasmtime::*;
+
+lazy_static! {
+    pub static ref ENGINE: Engine = Engine::default();
+    pub static ref STORE: Store<()> = Store::new(
+        &ENGINE,
+        ()
+    ); 
+}
